8866b1e9c9bb4fb81daa14ff39bb1d6a9541bc9b
BrettBrewster <bbrewster@sealanddesign.com>
2019-11-19 15:40:27 -0500

    Enhance habit functionality
    - Update insert_habit_log.php endpoint to update started habit rather than creating new row where appropriate
    - Show a half-empty hourglass (WIP) for each started instance
    - Develop no-further-action-today and no-further-action-this-window functionality/styles
    - Visibility on frequency_type (at least = greater than or equal to, exactly, at most = less than or equal to)

--- resources/ajax/insert_habit_log.php
+++ resources/ajax/insert_habit_log.php
@@ -12,13 +12,32 @@
     // Connect to DB
     $conn = connect_to_db();
 
+    // If status is set to Completed then check to see if there is a started habit log. If there is, update that status to 'Completed' rather than inserting new row
+    if ($status == 'Completed') {
+        $qry = "SELECT COUNT(*) FROM personal_wellness_habit_logs WHERE habit_id = $habit_id";
+        $res = $conn->query($qry);
+        $row = mysqli_fetch_row($res);
+        if ($row[0] > 0) {
+            $qry_update = "UPDATE personal_wellness_habit_logs SET status = 'Completed' WHERE habit_id = $habit_id AND status = 'Started' LIMIT 1;";
+            if ($conn->query($qry_update) === TRUE) {
+                $response_object->type = 'UPDATE';
+                $response_object->success = true;
+            } else {
+                $response_object->type = 'UPDATE';
+                $response_object->success = false;
+            }
+        }
+    }
+
     $qry = "INSERT INTO personal_wellness_habit_logs (habit_id, status) VALUES ($habit_id, '$status');";
     $response_object = new stdClass;
     // echo $qry;
     $response_object->qry = $qry;
     if ($conn->query($qry) === TRUE) {
+        $response_object->type = 'INSERT';
         $response_object->success = true;
     } else {
+        $response_object->type = 'INSERT';
         $response_object->success = false;
     }
     echo json_encode($response_object);
