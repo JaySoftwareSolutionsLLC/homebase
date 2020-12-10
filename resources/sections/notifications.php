709e3a76e8a5ecfeb5b218f7af122de27db32dbb
BrettBrewster <bbrewster@sealanddesign.com>
2020-12-10 12:30:11 -0500

    Comment out a few repeat notifications

--- resources/sections/notifications.php
+++ resources/sections/notifications.php
@@ -12,10 +12,10 @@
 
 	// Would be nice to break these out into a tasks and tasks_log table but I may need to research triggers to implement this. ie. If personal_day_info has expense review updated then trigger an update to tasks_log
 	$notifications[] = return_metric_based_notification_object($days_since_most_recent_expense_review, "Days Since Last Expense Review ", 0, 7, 0, 14, "N/A", "N/A", "Expense Review Overdue (" . date_format($most_recent_expense_review_dt, 'Y-m-d') . ")", "Expense Review Far Overdue (" . date_format($most_recent_expense_review_dt, 'Y-m-d') . ")", 15 ); // Check Schwab & BoA account history (as well as memory account of cash expenditure) to verify all expenses were logged correctly
-	$notifications[] = return_metric_based_notification_object($days_since_most_recent_upper_arm_circ_measurement, 'Days Since Last Upper Arm Measurement', 0, 14, 0, 28, "N/A", "N/A", "Upper Arm Circ Overdue", "Upper Arm Circ Far Overdue", 5 );
+	// $notifications[] = return_metric_based_notification_object($days_since_most_recent_upper_arm_circ_measurement, 'Days Since Last Upper Arm Measurement', 0, 14, 0, 28, "N/A", "N/A", "Upper Arm Circ Overdue", "Upper Arm Circ Far Overdue", 5 );
 	//$notifications[] = return_metric_based_notification_object($account_types['liquid cash'], 'Current Cash', 5400, 10800, 2700, 14000, "Cash on hand low", "Cash on hand very low", "Cash on hand high", "Cash on hand very high", 10 ); Notification hidden until dust settles with mom and dad house situation
-	$notifications[] = return_metric_based_notification_object($tricep_mo->hur, 'Tricep HUR', 0, 999, -24, 999, 'Tricep lift overdue', 'Tricep lift far overdue', '', '', 8);
-	$notifications[] = return_metric_based_notification_object($bicep_mo->hur, 'Bicep HUR', 0, 999, -24, 999, 'Bicep lift overdue', 'Bicep lift far overdue', '', '', 8);
+	// $notifications[] = return_metric_based_notification_object($tricep_mo->hur, 'Tricep HUR', 0, 999, -24, 999, 'Tricep lift overdue', 'Tricep lift far overdue', '', '', 8);
+	// $notifications[] = return_metric_based_notification_object($bicep_mo->hur, 'Bicep HUR', 0, 999, -24, 999, 'Bicep lift overdue', 'Bicep lift far overdue', '', '', 8);
 	// var_dump inside of hidden div for debugging and development purposes
 	hidden_var_dump($notifications); // Instantiated in epicor-web/common/resources.php
 ?>
