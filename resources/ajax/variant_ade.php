ee05349e04fc5a8b1644c6fce0c40f309ccf2d3c
BrettBrewster <bbrewster@sealanddesign.com>
2021-01-13 18:27:01 -0500

    Fix variant_ade.php bug (numbers were slightly off between php and ajax)

--- resources/ajax/variant_ade.php
+++ resources/ajax/variant_ade.php
@@ -5,14 +5,14 @@
 
     // Store posted exp_roi value in variable
     $category = $_POST['category'] ?? 'all';
-    $sd = $_POST['sd'] ?? date('2019-01-01'); // start date
+    $sd = $_POST['sd'] ?? date('2021-01-01'); // start date
     $ed = $_POST['ed'] ?? date('now'); // end date
     // Connect to DB
     $conn = connect_to_db();
     //echo "$category";
     // Determine current account values
     $exp = return_expenditure($conn, $sd, $ed, $category);
-    $days = return_days_between_dates($sd, $ed);
+    $days = return_days_between_dates($sd, $ed) + 1;
     $ade = $exp / $days;
 
     // Return age 60 ADW as html
