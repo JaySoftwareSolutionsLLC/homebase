02f82c08dd4c01f892d350cee973318dc911432c
BrettBrewster <bbrewster@sealanddesign.com>
2022-01-18 11:58:40 -0500

    Fix goals from old years

--- resources/sections/goals.php
+++ resources/sections/goals.php
@@ -63,13 +63,13 @@
 	}
 	else if ($year == '2019') {
 		
-		echo return_timed_goal_progress_bar_html('NW Cont.', 'goal-net-worth-cont', 0, ANNUAL_NET_WORTH_CONTRIBUTION_TARGET, $current_est_nw_contribution, '2019-01-01', '2019-12-31'); // Defined in homebase/resources/resources.php
+		echo return_timed_goal_progress_bar_html('NW Cont.', 'goal-net-worth-cont', 0, ANNUAL_NET_WORTH_CONTRIBUTION_TARGET, $current_est_nw_contribution, '2019-01-01', '2019-12-31', '2019-12-31', 'linear', null); // Defined in homebase/resources/resources.php
 		
-		echo return_timed_goal_progress_bar_html('Cert Hrs', 'goal-full-stack-certification', 0, SOFTWARE_DEV_TARGET_CERT_GOAL, $software_cert_hours, START_DATE_STRING_CERT_GOAL, END_DATE_STRING_CERT_GOAL, 'now', 'linear', null); // Defined in homebase/resources/resources.php
+		echo return_timed_goal_progress_bar_html('Cert Hrs', 'goal-full-stack-certification', 0, SOFTWARE_DEV_TARGET_CERT_GOAL, $software_cert_hours, START_DATE_STRING_CERT_GOAL, END_DATE_STRING_CERT_GOAL, END_DATE_STRING_CERT_GOAL, 'linear', null); // Defined in homebase/resources/resources.php
 		
-		echo return_timed_goal_progress_bar_html('Body Weight', 'goal-body-weight', STARTING_BODY_WEIGHT, BODY_WEIGHT_TARGET, $most_recent_body_weight, START_DATE_STRING_BODY_WEIGHT, '2019-12-31', 'now', /*'polynomial',*/ 'linear', null); // Defined in homebase/resources/resources.php
+		echo return_timed_goal_progress_bar_html('Body Weight', 'goal-body-weight', STARTING_BODY_WEIGHT, BODY_WEIGHT_TARGET, $most_recent_body_weight, START_DATE_STRING_BODY_WEIGHT, '2019-12-31', '2019-12-31', /*'polynomial',*/ 'linear', null); // Defined in homebase/resources/resources.php
 		
-		echo return_timed_goal_progress_bar_html('Arm Size', 'goal-arm-size', STARTING_UPPER_ARM_CIRC, UPPER_ARM_CIRC_TARGET, $most_recent_upper_arm_size, START_DATE_STRING_UPPER_ARM_CIRC, '2019-12-31', 'now', /*'polynomial',*/ 'linear', null); // Defined in homebase/resources/resources.php
+		echo return_timed_goal_progress_bar_html('Arm Size', 'goal-arm-size', STARTING_UPPER_ARM_CIRC, UPPER_ARM_CIRC_TARGET, $most_recent_upper_arm_size, START_DATE_STRING_UPPER_ARM_CIRC, '2019-12-31', '2019-12-31', /*'polynomial',*/ 'linear', null); // Defined in homebase/resources/resources.php
 		
 		//echo return_timed_goal_progress_bar_html('Mile Time', 'goal-mile-time', STARTING_MILE_TIME, MILE_TIME_TARGET, $best_mile_time, START_DATE_STRING_RUNNING, '2019-12-31', 'now', 'polynomial', null); // Defined in homebase/resources/resources.php
 
@@ -84,13 +84,13 @@
 	}
 	else if ($year == '2020') {
 		# Fourth Quarter
-		echo return_timed_goal_progress_bar_html('Family Habit App', 'family-app', 0, 9, 7, '2020-10-01', '2020-12-31', 'now');
+		echo return_timed_goal_progress_bar_html('Family Habit App', 'family-app', 0, 9, 7, '2020-10-01', '2020-12-31', '2020-12-31');
 		
-		echo return_timed_goal_progress_bar_html('Become Kratos', 'become-kratos', 0, 11, 5, '2020-10-01', '2020-12-31', 'now');
+		echo return_timed_goal_progress_bar_html('Become Kratos', 'become-kratos', 0, 11, 5, '2020-10-01', '2020-12-31', '2020-12-31');
 		
-		echo return_timed_goal_progress_bar_html('Yale SOWB', 'yale-sowb', 0, 8, 7, '2020-10-01', '2020-12-31', 'now');
+		echo return_timed_goal_progress_bar_html('Yale SOWB', 'yale-sowb', 0, 8, 7, '2020-10-01', '2020-12-31', '2020-12-31');
 		
-		echo return_timed_goal_progress_bar_html('MBA Apps', 'mba-apps', 0, 10, 9, '2020-10-01', '2020-12-31', 'now');
+		echo return_timed_goal_progress_bar_html('MBA Apps', 'mba-apps', 0, 10, 9, '2020-10-01', '2020-12-31', '2020-12-31');
 	}
 ?>
 
